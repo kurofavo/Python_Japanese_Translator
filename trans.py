07382e984f7225733229fb700e810a729522f449
Favour Numonde <favournumonde@gmail.com>
2023-01-20 08:46:53 +0100

    first commit

--- trans.py
+++ trans.py
@@ -1,12 +1,12 @@
 from translate import Translator
 translator = Translator(to_lang="ja")
 try:
-	with open("test.txt", mode="r", encoding='utf-8') as my_file:
-	    lll = my_file.read()
-	translation = translator.translate(lll)
-	with open("lol.txt", mode="w", encoding='utf-8') as my_file2:
-		my_file2.write(translation)
+    with open("filename", mode="r", encoding='utf-8') as my_file:
+        new = my_file.read()
+    translation = translator.translate(new)
+    with open("newfiilename", mode="w", encoding='utf-8') as my_file2:
+        my_file2.write(translation)
 except FileNotFoundError:
-	print("file could not be found")
+    print("file could not be found")
 except UnicodeEncodeError as err:
-	print(f"hey watch out{err}")
+    print(f"hey watch out{err}")
