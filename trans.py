6e2899f341ed911c087746f464202db3729e6c08
Favour Numonde <119840406+kurofavo@users.noreply.github.com>
2023-01-20 09:02:57 +0100

    Update trans.py

--- trans.py
+++ trans.py
@@ -1,9 +1,11 @@
 from translate import Translator
 translator = Translator(to_lang="ja")
 try:
+#next line "filename" is simply name of file to be translated
     with open("filename", mode="r", encoding='utf-8') as my_file:
         new = my_file.read()
     translation = translator.translate(new)
+#next line "newfilename" is where the translation will be written in
     with open("newfiilename", mode="w", encoding='utf-8') as my_file2:
         my_file2.write(translation)
 except FileNotFoundError:
